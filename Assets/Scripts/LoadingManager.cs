4b6bff9cd3e0296b53f009ec47fec7ae565a38af
cagdasbilgin <cagdasbilgin1@gmail.com>
2024-05-20 20:08:35 +0300

    loading panel for the game to load

---
+++ Assets/Scripts/LoadingManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+public class LoadingManager : MonoBehaviour
+{
+    [Inject] Player player;
+
+    [SerializeField] GameObject _loadingPanel;
+    [SerializeField] Slider _progressBar;
+    [SerializeField] TextMeshProUGUI _loadingTxt;
+    [SerializeField] float _totalDuration = 5f;
+
+    void Start()
+    {
+        StartLoading();
+    }
+
+    public void StartLoading()
+    {
+        StartCoroutine(LoadData());
+    }
+
+    IEnumerator LoadData()
+    {
+        float elapsedTime = 0f;
+
+        while (elapsedTime < _totalDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            float progress = Mathf.Clamp01(elapsedTime / _totalDuration);
+            _progressBar.value = progress;
+            _loadingTxt.text = "Loading... " + (int)(progress * 100) + "%";
+            yield return null;
+        }
+
+        _loadingPanel.SetActive(false);
+        player.StartSprint();
+    }
+}
